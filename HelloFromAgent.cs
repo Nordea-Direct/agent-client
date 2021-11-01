4a65309216419fde0648944310d517eaeb7aba43
Bjørn Moholt <bjorn.moholt@consult.nordea.com>
2021-11-01 15:48:05 +0100

    works

--- HelloFromAgent.cs
+++ HelloFromAgent.cs
@@ -1,29 +1,36 @@
-using System;
+using System.IO;
+using System.Net;
+using System.Text;
 
-namespace HelloNordea{
+namespace HelloNordea
+{
 
-    class HelloFromAgent { 
-        static void Main(string [] args) {
-            HttpWebRequest httpWebRequest22 = (HttpWebRequest)WebRequest.Create(url);
+    class HelloFromAgent
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest httpWebRequest22 = (HttpWebRequest)WebRequest.Create("https://test.direct.nordea.no/creditflow-agent/CFProxyWS");
 
-            // httpWebRequest22.Timeout = 600000;
             httpWebRequest22.Method = "POST";
             httpWebRequest22.ContentType = "application/xml";
-            httpWebRequest22.Headers.Add("Authorization", "Basic " + Base64Encode($"{userName}:{password}"));
+            httpWebRequest22.Headers.Add("Authorization", "Basic " + Base64Encode("gbunofinans_a2a:D+AsH2DFr?#y+rdw"));
+            
+            string documentInput = System.IO.File.ReadAllText(@"request.xml");
 
-            //X509Certificate2Collection certificates = new X509Certificate2Collection();
-            //certificates.Import(cert, certPWd, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
-
-            //httpWebRequest22.ClientCertificates = certificates;
-
-            NordeaProxy.ProcessExecuteAsync documentIn = new NordeaProxy.ProcessExecuteAsync();
-
-            documentIn.documentInput = CreateApplication(objSak, objSoknad, userName, password, true);
 
             using (StreamWriter streamWriter = new StreamWriter(httpWebRequest22.GetRequestStream(), Encoding.Default))
-                streamWriter.Write(documentIn.documentInput);
+                streamWriter.Write(documentInput);
 
             HttpWebResponse response = (HttpWebResponse)httpWebRequest22.GetResponse();
+
+            using(var reader = new StreamReader(response.GetResponseStream()))
+                File.WriteAllText("out", reader.ReadToEnd());
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
         }
     }
 }
