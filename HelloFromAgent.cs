eca67e20131fc832fb56db81012f1c5e76938414
Espen <espen.erlandsen@gmail.com>
2021-11-01 21:36:52 +0100

    changed to HttpClient (and got response)

--- HelloFromAgent.cs
+++ HelloFromAgent.cs
@@ -1,30 +1,45 @@
+using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HelloNordea
 {
 
     class HelloFromAgent
     {
-        static void Main(string[] args)
-        {
-            HttpWebRequest httpWebRequest22 = (HttpWebRequest)WebRequest.Create("https://test.direct.nordea.no/creditflow-agent/CFProxyWS");
+        static readonly HttpClient client = new HttpClient();
 
-            httpWebRequest22.Method = "POST";
-            httpWebRequest22.ContentType = "application/xml";
-            httpWebRequest22.Headers.Add("Authorization", "Basic " + Base64Encode("gbunofinans_a2a:D+AsH2DFr?#y+rdw"));
-            
+        static async Task Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+        
+            string uri = "https://test.direct.nordea.no/creditflow-agent/CFProxyWS";
             string documentInput = System.IO.File.ReadAllText(@"request.xml");
 
+            var stringContent = new StringContent(documentInput);
 
-            using (StreamWriter streamWriter = new StreamWriter(httpWebRequest22.GetRequestStream(), Encoding.Default))
-                streamWriter.Write(documentInput);
+            var response = await client.PostAsync(uri, stringContent);
+            string content = await response.Content.ReadAsStringAsync();
 
-            HttpWebResponse response = (HttpWebResponse)httpWebRequest22.GetResponse();
+            Console.WriteLine(content);
+            File.WriteAllText("out", content);
 
-            using(var reader = new StreamReader(response.GetResponseStream()))
-                File.WriteAllText("out", reader.ReadToEnd());
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+                
         }
 
         public static string Base64Encode(string plainText)
